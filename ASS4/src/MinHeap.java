d0e2668250e5b76f85074b8d049fe852a78f11fe
Black-Magician <jcardona652@gmail.com>
2015-04-16 13:37:56 -0400

    almost done minheap

--- ASS4/src/MinHeap.java
+++ ASS4/src/MinHeap.java
@@ -1,36 +1,108 @@
 
 public class MinHeap
 {
-	public MinHeap()
-	{
-		
-	}
-	
-	public boolean isEmpty()
-	{
-		boolean result = true;
-		
-		
-		
-		return result;
-	}
-	
-	public int insert(Object item)
-	{
-		
-	}
-	
-	
-	public boolean DeleteMin()
-	{
-		
-	}
-	
-	public int decreaseKey(int index, int value)
-	{
-		
-	}
-	
+	private int[] Heap;
+    private int size;
+    private int maxsize;
+ 
+    private static final int FRONT = 1;
+ 
+    public MinHeap(int maxsize)
+    {
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+ 
+    private int parent(int pos)
+    {
+        return pos / 2;
+    }
+ 
+    private int leftChild(int pos)
+    {
+        return (2 * pos);
+    }
+ 
+    private int rightChild(int pos)
+    {
+        return (2 * pos) + 1;
+    }
+ 
+    private boolean isLeaf(int pos)
+    {
+        if (pos >=  (size / 2)  &&  pos <= size)
+        { 
+            return true;
+        }
+        return false;
+    }
+ 
+    private void swap(int fpos, int spos)
+    {
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+ 
+    private void trickleDown(int pos)
+    {
+        if (!isLeaf(pos))
+        { 
+            if ( Heap[pos] > Heap[leftChild(pos)]  || Heap[pos] > Heap[rightChild(pos)])
+            {
+                if (Heap[leftChild(pos)] < Heap[rightChild(pos)])
+                {
+                    swap(pos, leftChild(pos));
+                    trickleDown(leftChild(pos));
+                }else
+                {
+                    swap(pos, rightChild(pos));
+                    trickleDown(rightChild(pos));
+                }
+            }
+        }
+    }
+ 
+    public void insert(int element)
+    {
+        Heap[++size] = element;
+        int current = size;
+ 
+        while (Heap[current] < Heap[parent(current)])
+        {
+            swap(current,parent(current));
+            current = parent(current);
+        }	
+    }
+ 
+    public void print()
+    {
+        for (int i = 1; i <= size / 2; i++ )
+        {
+            System.out.print(" PARENT : " + Heap[i] + " LEFT CHILD : " + Heap[2*i] 
+                + " RIGHT CHILD :" + Heap[2 * i  + 1]);
+            System.out.println();
+        } 
+    }
+ 
+    public void minHeap()
+    {
+        for (int pos = (size / 2); pos >= 1 ; pos--)
+        {
+            trickleDown(pos);
+        }
+    }
+ 
+    public int remove()
+    {
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--]; 
+        trickleDown(FRONT);
+        return popped;
+    }
 	
 	
 }
