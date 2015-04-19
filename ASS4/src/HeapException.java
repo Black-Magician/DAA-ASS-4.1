8f65ff49bd6316187af8cc18fc6d4b38e822ebb5
Black-Magician <jcardona652@gmail.com>
2015-04-19 10:47:54 -0400

    using github with eclipse is freaking annoying
    all these metadata and recomender files ar pissing me off lets see what
    happens when i commit all of em

--- ASS4/src/HeapException.java
+++
@@ -1,8 +0,0 @@
-
-public class HeapException extends java.lang.RuntimeException
-{
-	public HeapException(String s)
-	{
-		super(s);
-	}
-}
--- ASS4/src/git/AdjacencyList.java
+++ ASS4/src/git/AdjacencyList.java
@@ -3,17 +3,27 @@ package git;
 
 public class AdjacencyList
 {
-	private Node[] AL;
+	private Node[] AL;	
 	
 	public AdjacencyList(int numVerticies)
 	{
-		
+		AL = new Node[numVerticies];
 	}
 	
+	public Node[] getAdList()
+	{
+		return AL;
+	}
 	
+	public Node getNextatVertex(int index)
+	{
+		return AL[index].getNextNode();
+	}
 	
-	
-	
+	public Node getVertexatIndex(int index)
+	{
+		return AL[index];
+	}
 	
 	
 	
